0374bde1f8302a94108d88c83b8003ac8977a6e4
ArtsemiKot <kot_artemiy@mail.ru>
2023-11-10 23:53:06 +0300

    express-environment-pg

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
 const app = require('./src/app');
+const port = process.env.PORT;
 
-app.listen(3000, ()=>console.log('Server running on port 3000'));
+app.listen(port, ()=>console.log(`Server running on port ${port}`));
